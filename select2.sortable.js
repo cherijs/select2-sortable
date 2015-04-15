d0a4e67e0b189780708010bfceecbbc6c0a8174c
Artūrs Cirsis <arturs.cirsis@gmail.com>
2015-04-15 12:04:53 +0300

    Version update, to support select2 v4.0.0

--- select2.sortable.js
+++ select2.sortable.js
@@ -1,9 +1,9 @@
 /**
  * jQuery Select2 Sortable
  * - enable select2 to be sortable via normal select element
- * 
+ *
  * author      : Vafour
- * modified    : Kevin Provance (kprovance)
+ * modified by : Artūrs Cirsis
  * inspired by : jQuery Chosen Sortable (https://github.com/mrhenry/jquery-chosen-sortable)
  * License     : GPL
  */
@@ -21,114 +21,105 @@
                     return false;
                 }
 
-                var $select2 = $select.siblings('.select2-container');
-                var sorted;
+                var $select2 = $select.siblings('.select2-container'),
+                    unselected = [],
+                    sorted;
 
-                // Opt group names
-                var optArr = [];
-                
-                $select.find('optgroup').each(function(idx, val) {
-                    optArr.push (val);
+                $select.find('option').each(function () {
+                    !this.selected && unselected.push(this);
                 });
-                
-                $select.find('option').each(function(idx, val) {
-                    var groupName = $(this).parent('optgroup').prop('label');
-                    var optVal = this;
-                    
-                    if (groupName === undefined) {
-                        if (this.value !== '' && !this.selected) {
-                            optArr.push (optVal);
-                        }
-                    }
-                });
-                
-                sorted = $($select2.find('.select2-choices li[class!="select2-search-field"]').map(function () {
+
+                sorted = $($select2.find('li.select2-selection__choice').map(function () {
+                    // sorted = $($select2.find('.select2-selection__choice li[class!="select2-search-field"]').map(function () {
                     if (!this) {
                         return undefined;
                     }
-                    
-                    var id = $(this).data('select2Data').id;
-
+                    var id = $(this).data('data').id;
                     return $select.find('option[value="' + id + '"]')[0];
                 }));
- 
-                 sorted.push.apply(sorted, optArr);
-                
+
+
+
+                sorted.push.apply(sorted, unselected);
+
+
                 $select.children().remove();
                 $select.append(sorted);
-              });
+            });
 
             return $this;
         },
-        
         select2Sortable: function () {
             var args = Array.prototype.slice.call(arguments, 0);
             $this = this.filter('[multiple]'),
-                    validMethods = ['destroy'];
+            validMethods = ['destroy'];
 
             if (args.length === 0 || typeof (args[0]) === 'object') {
                 var defaultOptions = {
                     bindOrder: 'formSubmit', // or sortableStop
                     sortableOptions: {
-                        placeholder: 'ui-state-highlight',
-                        items: 'li:not(.select2-search-field)',
-                        tolerance: 'pointer'
+                        group: "select2_choices",
+                        sort: true,
+                        disabled: false,
+                        animation: 50,
+                        ghostClass: "sortable-ghost",
+                        // placeholder: 'ui-state-highlight',
+                        // items: 'li:not(.select2-search-field)',
+                        // tolerance: 'pointer'
                     }
                 };
-                
                 var options = $.extend(defaultOptions, args[0]);
 
                 // Init select2 only if not already initialized to prevent select2 configuration loss
+
                 if (typeof ($this.data('select2')) !== 'object') {
                     $this.select2();
                 }
 
                 $this.each(function () {
-                    var $select = $(this)
-                    var $select2choices = $select.siblings('.select2-container').find('.select2-choices');
+                    var $select = $(this),
+                        $select2choices = $select.siblings('.select2-container').find('.select2-selection__rendered');
 
                     // Init jQuery UI Sortable
                     $select2choices.sortable(options.sortableOptions);
 
                     switch (options.bindOrder) {
-                        case 'sortableStop':
-                            // apply options ordering in sortstop event
-                            $select2choices.on("sortstop.select2sortable", function (event, ui) {
-                                $select.select2SortableOrder();
-                            });
-                            
-                            $select.on('change', function (e) {
-                                $(this).select2SortableOrder();
-                            });
+
+
+                    case 'sortableStop':
+                        // apply options ordering in sortstop event
+                        $select2choices.on("sortstop.select2sortable", function (event, ui) {
+                            $select.select2SortableOrder();
+                        });
+                        $select.on('change', function (e) {
+                            $(this).select2SortableOrder();
+                        });
                         break;
-                        
-                        default:
-                            // apply options ordering in form submit
-                            $select.closest('form').unbind('submit.select2sortable').on('submit.select2sortable', function () {
-                                $select.select2SortableOrder();
-                            });
-                        break;
+                    default:
+
+                        // apply options ordering in form submit
+                        $select.closest('form').unbind('submit.select2sortable').on('submit.select2sortable', function () {
+                            $select.select2SortableOrder();
+
+                        });
                     }
+
                 });
-            }
-            else if (typeof (args[0] === 'string')) {
+            } else if (typeof (args[0] === 'string')) {
                 if ($.inArray(args[0], validMethods) == -1) {
                     throw "Unknown method: " + args[0];
                 }
-                
                 if (args[0] === 'destroy') {
                     $this.select2SortableDestroy();
                 }
             }
-            
             return $this;
         },
-        
         select2SortableDestroy: function () {
             var $this = this.filter('[multiple]');
             $this.each(function () {
-                var $select = $(this)
-                var $select2choices = $select.parent().find('.select2-choices');
+                var $select = $(this),
+                    $select2choices = $select.parent().find('.select2-selection__choice');
 
                 // unbind form submit event
                 $select.closest('form').unbind('submit.select2sortable');
@@ -139,7 +130,6 @@
                 // destroy select2Sortable
                 $select2choices.sortable('destroy');
             });
-            
             return $this;
         }
     });
